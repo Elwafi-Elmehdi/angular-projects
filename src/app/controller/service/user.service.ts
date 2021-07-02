f2e05e72b6ca1384674ec598d208729905d4a738
Mehdi <elwafi.courrier99@gmail.com>
2021-07-02 01:37:30 +0100

    updated User Service

--- src/app/controller/service/user.service.ts
+++ src/app/controller/service/user.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../model/user.model";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  private url = environment.url+'/users'
+
+  public register(user:User){
+    return this.http.post(this.url,user)
+  }
+  public login(user:User){
+    return this.http.post(this.url,user)
+  }
+  public readProfile() {
+    return this.http.get(this.url+'/me')
+  }
 }
