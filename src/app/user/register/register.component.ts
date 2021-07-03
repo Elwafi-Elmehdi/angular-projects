a02eeb46d77e505418d14a6e7807bfd334cd67c3
DAHBI AHMED <dahbiahmed1999@gmail.com>
2021-07-03 22:04:10 +0100

    front1

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -9,14 +9,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 export class RegisterComponent implements OnInit {
    constructor() {
   }
-  formregister=new FormGroup(
-    {
-      "fullname": new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z ]*')]),
-      "password": new FormControl('',Validators.required ),
-      "date": new FormControl('',Validators.required),
-      "email": new FormControl('',[Validators.required,Validators.email]),
-    }
-  );
+
   ngOnInit(): void {
 
   }
